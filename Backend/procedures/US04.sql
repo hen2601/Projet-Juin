49231550104c1ab8d708977ee0ac2d113d13ca76
hen2601 <58885506+hen2601@users.noreply.github.com>
2021-05-23 11:57:14 +0200

    Update US04.sql

--- Backend/procedures/US04.sql
+++ Backend/procedures/US04.sql
@@ -1,4 +1,4 @@
-ALTER PROCEDURE "DBA"."US04"(in firstName VARCHAR(50), in lastName VARCHAR(50), in born DATE, in sex CHAR(1))
+CREATE PROCEDURE "DBA"."US04"(in firstName VARCHAR(50), in lastName VARCHAR(50), in born DATE, in sex CHAR(1))
 BEGIN
     INSERT INTO tbPersonnes (prenom, nom, dateDeNaissance, sexe)
     VALUES (firstName, lastName, born, sex)
